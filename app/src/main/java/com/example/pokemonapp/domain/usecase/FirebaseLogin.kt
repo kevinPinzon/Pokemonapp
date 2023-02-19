90f26d7c8f54955d18a3753bb6036bafbbcea2a5
Kevin Pinzon <85967141+kevinPinzonLaureate@users.noreply.github.com>
2023-02-19 16:43:20 -0600

    getting user from realtimefirebase

--- app/src/main/java/com/example/pokemonapp/domain/usecase/FirebaseLogin.kt
+++ app/src/main/java/com/example/pokemonapp/domain/usecase/FirebaseLogin.kt
@@ -1,20 +1,24 @@
 package com.example.pokemonapp.domain.usecase
 
+import com.example.pokemonapp.domain.model.User
 import com.example.pokemonapp.domain.repository.AuthRepository
+import com.example.pokemonapp.domain.repository.UserRepository
 import com.example.pokemonapp.utils.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class FirebaseLogin @Inject constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository
 ) {
 
-    suspend operator fun invoke(email: String, password: String): Flow<Resource<Boolean>> = flow {
+    suspend operator fun invoke(email: String, password: String): Flow<Resource<User>> = flow {
         emit(Resource.Loading)
-        val isLoginSuccessfully = authRepository.login(email, password)
-        if (isLoginSuccessfully) {
-            emit(Resource.Success(true))
+        val userUID = authRepository.login(email, password)
+        if (userUID.isNotEmpty()) {
+            val user: User = userRepository.getUser(userUID)
+            emit(Resource.Success(user))
             emit(Resource.Finished)
         } else {
             emit(Resource.Error("Login error"))
--- app/src/main/java/com/example/pokemonapp/ui/sigin/SignInViewModel.kt
+++ app/src/main/java/com/example/pokemonapp/ui/sigin/SignInViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pokemonapp.domain.model.User
 import com.example.pokemonapp.domain.usecase.FirebaseLogin
 import com.example.pokemonapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,8 +18,8 @@ class SignInViewModel @Inject constructor(
     private val loginUseCase: FirebaseLogin
 ): ViewModel() {
 
-    private val _loginState: MutableLiveData<Resource<Boolean>> = MutableLiveData()
-    val loginState: LiveData<Resource<Boolean>>
+    private val _loginState: MutableLiveData<Resource<User>> = MutableLiveData()
+    val loginState: LiveData<Resource<User>>
         get() = _loginState
 
     fun login(email: String, password: String) {
